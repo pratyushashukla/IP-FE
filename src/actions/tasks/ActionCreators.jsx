27b646e5d172c39d677497e19a9a107dd8e2260b
suraj2597 <surajpatel7520@gmail.com>
2024-10-26 16:49:37 -0400

    UI/API Integration - delete task

--- src/actions/tasks/ActionCreators.jsx
+++ src/actions/tasks/ActionCreators.jsx
@@ -4,10 +4,24 @@ import { TASKSDATA } from "./Actions";
 export const GET_TASKS_DATA = () => async (dispatch) => {
   try {
     const apiResponse = await ApiService.get(`/tasks`);
-    if (apiResponse) {
+    if (apiResponse.status == 200) {
       dispatchAction(dispatch, TASKSDATA, apiResponse.data);
     }
   } catch (error) {
     handleNetworkError(error);
   }
 };
+
+export const DELETE_TASKS = (id, handleCloseMenu) => async (dispatch) => {
+  try {
+    const apiResponse = await ApiService.delete(`/tasks/${id}`);
+    console.log("apiResponse",apiResponse);
+    if (apiResponse.status == 200) {
+      dispatch(GET_TASKS_DATA());
+      handleCloseMenu();
+    }
+  } catch (error) {
+    handleNetworkError(error);
+  }
+};
+
