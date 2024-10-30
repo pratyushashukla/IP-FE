e21bfacaba1456a50754b23504755ef5c211d76f
pratyushashukla <pratyusha.shukla212@gmail.com>
2024-10-30 15:24:36 -0400

    Added folder structure - Visitor CRUD

--- src/components/visit/Visit.jsx
+++ src/components/visit/Visit.jsx
@@ -4,6 +4,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Visitors from "./manageVisitors/Visitors";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -90,7 +91,7 @@ export default function Visit() {
         Appointments
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Visitors
+        <Visitors />
       </TabPanel>
     </Box>
   );
---
+++ src/components/visit/manageVisitors/Visitors.jsx
@@ -0,0 +1,10 @@
+const Visitors = () => {
+  return (
+    <h2>
+      Visitors This folder structure is similar to task management and inmate
+      management. follow the same for this manage visitors folder. There are already folders created to the same.
+    </h2>
+  );
+};
+
+export default Visitors;
